68dfb2c27468219a82a5ef88f8981ee7e1ee9038
Abhishek Kumar <abhikr.ak.2001@gmail.com>
2024-03-10 23:15:24 +0530

    first commit

---
+++ chapter 3/script.js
@@ -0,0 +1,58 @@
+// var a = 12;
+// console.log(a, "answer1");
+
+// if (true) {
+//   var a = 99;
+//   console.log(a, "answer 2");
+// }
+
+// for (let i = 0; i < 1; i++) {
+//   var a = 100;
+//   console.log(a);
+// }
+
+// console.log(a);
+
+// var obj = {
+//   name: "utkarsh",
+// };
+
+// console.log(obj);
+
+// function user() {
+//   var obj = {
+//     name: "amit",
+//   };
+//   console.log(obj);
+// }
+
+// user();
+
+// console.log(obj);
+
+// var a = 12;
+// console.log(a);
+
+// function abc() {
+//   console.log(a);
+//   var a = 99;
+//   console.log(a);
+// }
+// abc();
+
+// console.log(a);
+
+// let a = 99;
+// console.log(a);
+
+// if (true) {
+//   let a = 12;
+//   console.log(a);
+// }
+
+// console.log(a);
+
+let a = 99;
+console.log(a);
+a = 90;
+console.log(a);
